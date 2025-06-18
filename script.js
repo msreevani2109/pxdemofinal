3da1d83b711cc7a0b052af6ddec4b85daabef2c8
gs-msreevani <msreevani@gainsight.com>
2025-06-18 12:26:29 +0530

    adding comment

--- script.js
+++ script.js
@@ -33,9 +33,9 @@ const registerBtn = document.querySelector("#btn-register");
 const loginBtn = document.querySelector("#btn-login");
 const btnPopup = document.querySelector(".btnLgin-popup");
 const iconClose = document.querySelector(".icon-close");
-btnPopup.addEventListener("click", () => {
-  wrapper.classList.add("active-popup");
-});
+// btnPopup.addEventListener("click", () => {
+//   wrapper.classList.add("active-popup");
+// });
 
 registerLink.addEventListener("click", () => {
   wrapper.classList.add("active");
